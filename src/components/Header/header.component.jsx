3283a269c1fce89591f41d27067522f5a95f997b
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-07-07 13:40:44 -0300

    added styles to search box

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import logoMeli from '../../assets/images/Logo_ML.png';
+import searchIcon from '../../assets/images/ic_Search@2x.png'
 
 import './header.styles.scss';
 
@@ -10,10 +11,14 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logo-search">
-        <img src={logoMeli} alt="logo do mercado livre" />
+        <img className='logo' src={logoMeli} alt="logo do mercado livre" />
         <form className="search-form">
-          <input onChange={((e) => setSearchField(e.target.value))} placeholder="Nunca dejes de buscar" />
-          <input type='submit' value=''/>
+          <div className="search-box">
+            <input onChange={((e) => setSearchField(e.target.value))} placeholder="Nunca dejes de buscar" name="q" />
+            <button className='submit-button' type='submit'>
+              <img className='search-icon' src={searchIcon} alt="search icon" />
+            </button>
+          </div>
         </form>
       </div>
     </div>
