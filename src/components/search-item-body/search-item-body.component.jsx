fd20caa0c50f5410a6d4f4e4172d03f31c6d6792
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-07-09 18:41:36 -0300

    added search feature using mercado libre api

--- src/components/search-item-body/search-item-body.component.jsx
+++ src/components/search-item-body/search-item-body.component.jsx
@@ -4,21 +4,29 @@ import shippingIcon from '../../assets/images/ic_shipping@2x.png'
 
 import './search-item-body.styles.scss';
 
-const SearchItemBody = () => (
+const SearchItemBody = ({item}) => {
+  const { price, accepts_mercadopago: acceptsMercadopago, thumbnail_id: imageID, title, address } = item
+
+  return(
     <div className="item-body">
       <div className="image-price-description">
         <div className='item-image'
           style={{
-            backgroundImage: `url("https://images.unsplash.com/photo-1510166218561-8b0b8df7c887?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=511&q=80")`
+            backgroundImage: `url("https://http2.mlstatic.com/D_NQ_NP_${imageID}-O.webp")`
           }}>
         </div>
         <div className="price-description">
           <div className="price">
-            <div className='value'>$1.980</div>
-            <img className='shipping' src={shippingIcon} alt="this item has shipping" />
+            <div className='value'>$ {price}</div>
+            {
+              acceptsMercadopago ? 
+              (<img className='shipping' src={shippingIcon} alt="this item has shipping" />) 
+              : (null)
+            }
+            
           </div>
           <div className="description">
-            Apple Ipod Touch 5g 16gb Negro Igual A Nuevo
+            {title}
           </div>
           <div className="status">
             Completo Unico!
@@ -26,9 +34,10 @@ const SearchItemBody = () => (
         </div>
       </div>
       <div className="city">
-        Capital Federal
+        {address.state_name}
       </div>
     </div>
-);
+  )
+};
 
 export default SearchItemBody;
