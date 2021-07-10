0b7de7595ec900c08236a4e7fc0e472ae670f260
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-07-10 01:57:24 -0300

    added api to show page to show details of an item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1 className='call-to-action'>
+      Haga una búsqueda
+    </h1>
+  </div>
+);
+
+export default HomePage
