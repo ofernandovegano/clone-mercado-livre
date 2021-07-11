811c553ff2daaeee9e0ef5dcb0940bbe03bd0701
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-07-11 00:08:54 -0300

    connected with backend using microservices

--- src/components/search-item-body/search-item-body.component.jsx
+++ src/components/search-item-body/search-item-body.component.jsx
@@ -5,28 +5,21 @@ import shippingIcon from '../../assets/images/ic_shipping@2x.png'
 import './search-item-body.styles.scss';
 
 const SearchItemBody = ({item}) => {
-  const { 
-    price,
-    accepts_mercadopago: acceptsMercadopago,
-    thumbnail_id: imageID,
-    title,
-    address,
-    condition
-  }= item
+  const { price, free_shipping, picture, title, address, condition }= item
 
   return(
     <div className="item-body">
       <div className="image-price-description">
         <div className='item-image'
           style={{
-            backgroundImage: `url("https://http2.mlstatic.com/D_NQ_NP_${imageID}-O.webp")`
+            backgroundImage: `url(${picture})`
           }}>
         </div>
         <div className="price-description">
           <div className="price">
-            <div className='value'>$ {price}</div>
+            <div className='value'>$ {price.amount}</div>
             {
-              acceptsMercadopago ? 
+              free_shipping ? 
               (<img className='shipping' src={shippingIcon} alt="this item has shipping" />) 
               : (null)
             }
@@ -41,7 +34,7 @@ const SearchItemBody = ({item}) => {
         </div>
       </div>
       <div className="city">
-        {address.state_name}
+        {address}
       </div>
     </div>
   )
--- src/pages/show/show.component.jsx
+++ src/pages/show/show.component.jsx
@@ -1,34 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { apiShowItem } from '../../service/api';
+import { apiBackend } from '../../service/api';
 
 import './show.styles.scss';
 
 const Show = () => {
   const params = useParams();
-  const [item, setItem] = useState(null);
-  const [description, setDescription] = useState("")
+  const [item, setItem] = useState([]);
 
   useEffect(()=> {
-    apiShowItem.get(params.id).then(response => {
-      setItem(response.data)
-      console.log(response.data)
-    })
-    
-    apiShowItem.get(`${params.id}/description`).then(response => {
-      setDescription(response.data.plain_text)
+    apiBackend.get(`/${params.id}`).then(response => {
+      setItem(response.data.item)
+      console.log(response.data.item)
     })
   },[params.id])
 
-
-
+  const { price, picture, title, condition, sold_quantity, description } = item
+  
   return(
   <div className="show-page">
     <div className="item">
       <div className="image-description">
         <div className='item-image'
-          style={{ backgroundImage: `url(${item?.pictures[0].secure_url})` }}>
+          style={{ backgroundImage: `url(${picture?.secure_url})` }}>
         </div>
         <div className="description">
           <h3 className='description-title'>Descripción del producto</h3>
@@ -38,11 +33,11 @@ const Show = () => {
 
       <div className="call-to-action">
         <p>
-          {item?.condition === "new" ? "Nuevo" : "Usado"} - {item?.sold_quantity} vendidos
+          {condition === "new" ? "Nuevo" : "Usado"} - {sold_quantity} vendidos
         </p>
-        <h2>{item?.title}</h2>
+        <h2>{title}</h2>
         <div className="price">
-          $ {Math.floor(item?.price)}<span>{(item?.price % 1).toFixed(2).substring(2)}</span>
+          $ {price?.amount}<span>{price?.decimals}</span>
         </div>
         <button>Comprar</button>
       </div>
